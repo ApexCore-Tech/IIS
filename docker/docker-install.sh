b61954d0e942800b798a69d239c159bb22eb94b6
J1nH4ng <2838080432@qq.com>
2025-10-24 16:05:01 +0800

    :art: fix: 修改脚本内容

--- docker/docker-install.sh
+++ docker/docker-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Docker 与 Docker Compose 的一键安装脚本
 #
@@ -8,6 +8,8 @@
 # -u: 使用未定义变量时报错
 # -o pipefail: 管道中任何命令失败都算失败
 set -euo pipefail
+
+export SHELLOPTS
 
 readonly TEXT_BOLD='\033[1m'
 readonly TEXT_RED='\033[0;31m'
@@ -65,7 +67,7 @@ function utils_echo_icon() {
 function utils_get_scripts_metadata() {
   local script_name="${0##*/}"
   local script_dir=""
-  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
   local script_version="0.1.0-alpha+build.20251016"
 
   readonly script_name
@@ -167,7 +169,7 @@ function utils_found_pm() {
   for pm in "${package_managers[@]}"; do
     if command -v "${pm}" &> /dev/null; then
       found_pm="${pm}"
-      log_success "找到包管理器：${pm}"
+      log_success "$(gettext 'Find Package Manager'): ${pm}"
       break
     fi
   done
